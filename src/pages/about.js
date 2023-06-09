4337d649d0c27b33ba8e4928e731c65c669e4c4b
geraldyeokj <geraldyeokj@gmail.com>
2023-06-08 21:46:12 -0500

    tailwind added

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
 	return (
 		<div>
-			<h1>
+			<h1 className="py-5 px-5 text-4xl">
 				GeeksforGeeks is a Computer
 				Science portal for geeks.
 			</h1>
